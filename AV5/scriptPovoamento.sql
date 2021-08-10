eae9224822ec1366efeae901ff717ee287c0bc77
Matheus Lucena <47263211+matheusepitacio@users.noreply.github.com>
2021-08-10 19:00:06 -0300

    Iniciando povoamento

---
+++ AV5/scriptPovoamento.sql
@@ -0,0 +1,22 @@
+--POVOAMENTO DAS RELIQUIAS
+
+INSERT INTO tb_reliquia VALUES (tp_reliquia(1, 'Osso de Tiranossauro', 'Fóssil', '100000'));
+INSERT INTO tb_reliquia VALUES (tp_reliquia(2,'O Santo Graal','Objeto Preservado','2021'));
+INSERT INTO tb_reliquia VALUES (tp_reliquia(3, 'Crânio do Primeiro Humano', 'Fóssil', '80000'));
+INSERT INTO tb_reliquia VALUES (tp_reliquia(4, 'Vértebra de Saurópode','Fóssil','120000000'));
+INSERT INTO tb_reliquia VALUES (tp_reliquia(5, 'Excalibur', 'Objeto Preservado', '1500'));
+INSERT INTO tb_reliquia VALUES (tp_reliquia(6, 'Olho de thundera','Objeto Preservado','1900'));
+INSERT INTO tb_reliquia VALUES (tp_reliquia(7, 'Chápeu de Palha', 'Objeto Preservado', '100'));
+INSERT INTO tb_reliquia VALUES (tp_reliquia(8, 'Cauda de réptil', 'Fóssil', '150000'));
+INSERT INTO tb_reliquia VALUES (tp_reliquia(9, 'Dedo de Ryomen Sukuna', 'Fóssil', '2000'));
+INSERT INTO tb_reliquia VALUES (tp_reliquia(10, 'Garra de argentavis','Fóssil','11630000'));
+INSERT INTO tb_reliquia VALUES (tp_reliquia(11, 'Espada de Arthur', 'Objeto preservado', '1800'));
+INSERT INTO tb_reliquia VALUES (tp_reliquia(12, 'Espada do Rei Destruído', 'Objeto Preservado', '5000'));
+INSERT INTO tb_reliquia VALUES (tp_reliquia(13, 'Escudo Viking', 'Objeto Preservado', '500'));
+INSERT INTO tb_reliquia VALUES (tp_reliquia(14, 'Casco do Holandês Voador', 'Objeto Preservado','500'));
+INSERT INTO tb_reliquia VALUES (tp_reliquia(15, 'Portão de El Dourado','Objeto Preservado','1200'));
+INSERT INTO tb_reliquia VALUES (tp_reliquia(16, 'Escudo de Centurião Romano', 'Objeto Preservado', '2000'));
+INSERT INTO tb_reliquia VALUES (tp_reliquia(17, 'Dente de Megalodonte','Fóssil','5000000'));
+INSERT INTO tb_reliquia VALUES (tp_reliquia(18, 'Água de Atlântida', 'Objeto Preservado', '5000'));
+INSERT INTO tb_reliquia VALUES (tp_reliquia(19, 'Cabeça do Ancestral Comum','Fóssil','600000000'));
+INSERT INTO tb_reliquia VALUES (tp_reliquia(20, 'Costela de Alienígena Pré-Histórico', 'Fóssil', '500000'));
